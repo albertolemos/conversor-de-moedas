5668ee6ed9c4db16426726ca48e0dbe68f59037e
Alberto Lemos <albertolemos@gmail.com>
2025-02-25 16:31:06 -0300

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Alberto Lemos on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
